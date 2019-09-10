e5c318d4d4883f932c854cacf5761aaf4e98fa06
Krissy Lewis <kristianlewis03@gmail.com>
2019-09-10 22:58:39 +0000

    added toggleClass in order to change the colour of the counter when there are too many characters in the textarea

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,6 +2,13 @@ $(document).ready(function() {
   // using keyup to evaluate the textarea for character counter
   $('#tweet-text').on('keyup', function() {
     let charCount = 140 - this.value.length;
+    
+    // decides when to turn '.negCounter' on and off
+    if (charCount < 0) {
+        $('.counter').toggleClass('negCounter', true);
+    } else {
+        $('.counter').toggleClass('negCounter', false);
+    }
     $('.counter').text(`${charCount}`);
   });
 });
