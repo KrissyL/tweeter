c75b4cb3515d25c906d056799d657d706cf0c50d
Krissy Lewis <kristianlewis03@gmail.com>
2019-09-10 23:36:50 +0000

    switched from a straight '.counter' class call to traversing through the siblings of textarea to find the class '.counter'

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,14 +2,12 @@ $(document).ready(function() {
   // using keyup to evaluate the textarea for character counter
   $('#tweet-text').on('keyup', function() {
     let charCount = 140 - this.value.length;
-    
     // decides when to turn '.negCounter' on and off
     if (charCount < 0) {
-        $('.counter').toggleClass('negCounter', true);
+        $(this).siblings('.counter').toggleClass('negCounter', true);
     } else {
-        $('.counter').toggleClass('negCounter', false);
+        $(this).siblings('.counter').toggleClass('negCounter', false);
     }
-    $('.counter').text(`${charCount}`);
+    $(this).siblings('.counter').text(`${charCount}`);
   });
 });
-
