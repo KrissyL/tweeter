a8ac6edbcd75c282cae582c61fa9e7785e71b58b
Krissy Lewis <kristianlewis03@gmail.com>
2019-09-13 07:26:28 +0000

    final check through of code before preparing to submit

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,6 +4,7 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(() => { //will not run any code until the document is loaded
+  loadTweets();
   showNewTweet();
   fetchTweets();
   hideError();
@@ -67,34 +68,65 @@ const loadTweets = function() {
 
 //renders all tweets
 const renderTweets = function(tweets) {
-    for (const tweet of tweets) {
-      createTweetElement(tweet);
-    }
-  };
+  for (const tweet of tweets) {
+    createTweetElement(tweet);
+  }
+};
 
+  
 //create a new tweet
-const createTweetElement = function(data) {
+const createTweetElement = function(data) {  
   //template for all new tweets
   const $tweet = 
   $("<article>").addClass("tweet")
-  .append(
-    $("<header>")
+    .append($("<header>")
       .append(
       ($("<aside>").addClass("hide").text(data.user.handle))//appended inside header
       )
-    .append(
-      ($("<figure>").append(`<img src=${data.user.avatars}>`)//appended inside header
-      .append(data.user.name))//user name with avatar centered to it
+      .append(
+        ($("<figure>").append(`<img src=${data.user.avatars}>`)//appended inside header
+        .append(data.user.name))//user name with avatar centered to it
+      )
     )
-  )
-  .append($("<p>").text(data.content.text))//tweet text
-  .append(
-    $("<footer>").append(
-      ($("<time>").text(data.created_at))//time it was posted at (to be modified)
-      .append(`<img src="/images/icons/tweet-icons.png">`)
-    )
-  );
-  $('#tweets-container').prepend($tweet);//add new tweet to the tweets-container in index.html
+    .append($("<p>").text(data.content.text))//tweet text
+    .append($("<footer>")
+      .append(
+        ($("<time>").text(convertDate(data.created_at)))//time it was posted at (to be modified)
+        .append($(`<img class="hide" src="/images/icons/tweet-icons.png">`))//dummy icons that appear on mouse over
+      )
+    );
+        $('#tweets-container').prepend($tweet);//add new tweet to the tweets-container in index.html
+}          
+
+//gets the date from the timestamp
+const convertDate = function(timestamp) {
+  const currentTime = new Date (); //current date
+  const convertCurrent = currentTime.getTime(); //converted to timestamp
+  
+  const minute = 60 * 1000;
+  const hour = minute * 60;
+  const day = hour * 24;
+  const month = day * 30;
+  const year = day * 365;
+  
+  const timeSince = convertCurrent - timestamp;
+
+  if (timeSince < minute) {
+        return Math.round(timeSince/1000) + ' seconds ago';   
+  }
+  else if (timeSince < hour) {
+        return Math.round(timeSince/minute) + ' minutes ago';   
+  }
+  else if (timeSince < day ) {
+        return Math.round(timeSince/hour) + ' hours ago';   
+  }
+  else if (timeSince < month) {
+      return 'approximately ' + Math.round(timeSince/day) + ' days ago';   
+  }
+  else if (timeSince < year) {
+      return 'approximately ' + Math.round(timeSince/month) + ' months ago';   
+  }
+  else {
+      return 'approximately ' + Math.round(timeSince/year ) + ' years ago';   
+  }  
 }
-
-
