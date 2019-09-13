922e54774cb4922abf94e9664ec8c57015aa98b3
Krissy Lewis <kristianlewis03@gmail.com>
2019-09-13 04:18:19 +0000

    cleaned up the code and removed the character alerts

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,23 +1,18 @@
 $(document).ready(function() {
-  $('#new-tweet-form').on('submit', (() => {
-    if ($('textarea').val() === "") { // prevents submission of empty strings
-      alert("can't submit and empty Tweet, write something!");
-    }
-  }));
-  
+  checkChars();
+}); 
+
+const checkChars = function() {
   // using keyup to evaluate the textarea for character counter
   $('#tweet-text').on('keyup', function() {
-    let charCount = 140 - this.value.length;
-    
+    let charCount = 140 - this.value.length;    
     // decides when to turn '.negCounter' on and off
     if (charCount < 0) {
-        $(this).siblings('.counter').toggleClass('negCounter', true);
-        $('#new-tweet-form').on('submit', (() => { alert('Your tweet has too many characters!')}));// prevents a submit w/ too many chars
+      $(this).siblings('.counter').toggleClass('negCounter', true);
+      
     } else {
-        $(this).siblings('.counter').toggleClass('negCounter', false);
-        
+      $(this).siblings('.counter').toggleClass('negCounter', false);     
     }
     $(this).siblings('.counter').text(`${charCount}`);
   });
-  
-});
+}
